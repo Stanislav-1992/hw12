5a46691ea145828d2af98ffd09dbdc06bf060eef
Stanislav <s.tyulkov@gmail.com>
2024-03-01 17:45:04 +0300

    hw13

--- src/Author.java
+++ src/Author.java
@@ -5,12 +5,26 @@ public class Author {
         this.name = name;
         this.nameOne = nameOne;
     }
-
     public String getName() {
-        return  this.name;
+        return this.name;
     }
-
     public String getNameOne() {
         return this.nameOne;
+    }
+    @Override
+    public String toString() {
+        return "Автор: " + "имя - " + name + ", фамилия - " + nameOne;
+    }
+    @Override
+    public boolean equals(Object other) {
+        if (this.getClass() != other.getClass()) {
+            return false;
+        }
+        Author author = (Author) other;
+        return name.equals(author.name);
+    }
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(nameOne);
     }
 }
--- src/Book.java
+++ src/Book.java
@@ -8,11 +8,9 @@ public class Book {
         this.author = author;
         this.releaseYear = releaseYear;
     }
-
     public String getTitle() {
         return this.title;
     }
-
     public Author getAuthor() {
         return this.author;
     }
@@ -24,6 +22,20 @@ public class Book {
     public void setReleaseYear(int releaseYear) {
         this.releaseYear = releaseYear;
     }
-
+    @Override
+    public String toString() {
+        return "Книга: " + title + "; " + author + "; релиз: " + releaseYear;
+    }
+    @Override
+    public boolean equals(Object other) {
+        if (this.getClass() != other.getClass()) {
+            return false;
+        }
+        Book book = (Book) other;
+        return title.equals(book.title);
+    }
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(releaseYear);
+    }
 }
-
